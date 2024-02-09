377eb9a2120744d18a7584be6651d58b1668fe4a
Rachel Danover <rdano001@xe-10.cs.ucr.edu>
2024-02-09 12:33:51 -0800

    added IS A relationship

--- Phase_2_script.sql
+++ Phase_2_script.sql
@@ -9,6 +9,6 @@ DROP TABLE IF EXISTS Warehouse CASCADE;
 CREATE TABLE Storefront (StoreID INTEGER UNIQUE NOT NULL,Latitude DECIMAL(8,6) NOT NULL,Longitude DECIMAL(9,6) NOT NULL , DateEstablished DATE, PRIMARY KEY(StoreID));
 CREATE TABLE Product (ProductName char(30) NOT NULL, NumberOfUnits INTEGER NOT NULL, PricePerUnit INTEGER NOT NULL, Description char(100), ImageURL char(100));
 CREATE TABLE User (UserID INTEGER UNIQUE NOT NULL, Password char(11) NOT NULL, Name char(50) NOT NULL, Email char(50), PRIMARY KEY(UserID));
-CREATE TABLE Customer (CreditScore INTEGER, Latitude decimal(8,6) NOT NULL, Longitude decimal(9,6) NOT NULL);
-CREATE TABLE Manager (Degree char(20), Salary INTEGER NOT NULL);
+CREATE TABLE Customer (UserID INTEGER UNIQUE NOT NULL,CreditScore INTEGER, Latitude decimal(8,6) NOT NULL, Longitude decimal(9,6) NOT NULL, PRIMARY KEY(UserID), FOREIGN KEY(UserID) REFERENCES User(UserID));
+CREATE TABLE Manager (UserID INTEGER UNIQUE NOT NULL, Degree char(20), Salary INTEGER NOT NULL, PRIMARY KEY(UserID), FOREIGN KEY(UserID) REFERENCES User(UserID));
 CREATE TABLE Warehouse (WarehouseID INTEGER UNIQUE NOT NULL, Area INTEGER, Latitude decimal(8,6) NOT NULL, Longitude decimal(9,6) NOT NULL);
